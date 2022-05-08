b1b60ce8d93e99fd987783671a5ee0f42232614f
raja Wajahat <rjwaji110@gmail.com>
2022-05-09 02:23:04 +0500

    moved app object to make it according to the docker file statement

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,1 @@
-from fastapi import FastAPI
 
-server = FastAPI()
--- app/main.py
+++ app/main.py
@@ -1,22 +1,24 @@
 from fastapi import Request, Body
+from fastapi import FastAPI
 
-from app import server
 from .controller import post_event
 
+app = FastAPI()
 
-@server.get("/health")
+
+@app.get("/health")
 def health_check():
     return {"status": True}
 
 
-@server.post("/events_webhook")
+@app.post("/events_webhook")
 def events_webhook(request: Request, payload=Body(...)):
     events_list = payload.get("mandrill_events")
     response = post_event(events_list)
     return response
 
 
-@server.get("/events/{event_id}")
+@app.get("/events/{event_id}")
 def events(request: Request, event_id: str):
     response = get_event(event_id)
     return response
